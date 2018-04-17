052113b11e9a27cc5f66f34ebd6b5bda42604624
Andry <andry.jose.v@gmail.com>
2018-04-16 20:31:46 -0400

    16/4/2018 8;31pm

--- app/src/main/java/com/example/andry/platzigram/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/example/andry/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -1,6 +1,7 @@
 package com.example.andry.platzigram.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.example.andry.platzigram.R;
 import com.example.andry.platzigram.model.Picture;
+import com.example.andry.platzigram.view.PictureDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -40,7 +42,12 @@ public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdap
         holder.likeNumberCard.setText(picture.getLike_number());
         Picasso.get().load(picture.getPicture()).into(holder.pictureCard);
 
-
+        holder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+                activity.startActivity(intent);            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/andry/platzigram/view/PictureDetailActivity.java
+++ app/src/main/java/com/example/andry/platzigram/view/PictureDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.andry.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.andry.platzigram.R;
 
@@ -11,5 +13,12 @@ public class PictureDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
+    }
+
+    public void showToolbar(String tittle, boolean upButton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
